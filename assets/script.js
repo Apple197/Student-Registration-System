d5b7da818225e7bd5065f989e92b93c4e3bc3a11
Nazrul Hasan <appleios986@gmail.com>
2026-03-10 00:14:34 +0530

    I am working on display student details

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,23 @@
-console.log("Hello, World!"); 
+const studentForm = document.querySelector("#srs-form form");
+let studentData = JSON.parse(localStorage.getItem("students")) || [];
+studentForm.addEventListener("submit", formSubmissionHandler);
+
+console.log(studentData);
+
+function formSubmissionHandler(e) {
+  e.preventDefault();
+  let formDetails = new FormData(e.target);
+  formDetails = Object.fromEntries(formDetails.entries());
+  studentAdd(formDetails);
+  e.target.reset();
+  displayStudentDetail();
+}
+
+function studentAdd(student) {
+  studentData.push(student);
+  localStorage.setItem("students", JSON.stringify(studentData));
+}
+
+function displayStudentDetail() {
+  const studentInfo = JSON.parse(localStorage.getItem("students"));
+}
